59465e63b3a1ac4c9b7ddb92c26717f10b9839bd
G Singh <77468753+gursantsingh@users.noreply.github.com>
2023-07-25 19:56:04 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,28 @@
+// Create web server
+// To run: node comments.js
+// To test: curl -d "comment=Hello World" http://localhost:8000/comments
+// To test: curl -d "comment=Hello World" http://localhost:8000/comments.json
+// To test: curl -d "comment=Hello World" http://localhost:8000/comments.xml
+// To test: curl -d "comment=Hello World" http://localhost:8000/comments.html
+// To test: curl -d "comment=Hello World" http://localhost:8000/comments.txt
+// To test: curl -d "comment=Hello World" http://localhost:8000/comments.csv
+// To test: curl http://localhost:8000/comments
+// To test: curl http://localhost:8000/comments.json
+// To test: curl http://localhost:8000/comments.xml
+// To test: curl http://localhost:8000/comments.html
+// To test: curl http://localhost:8000/comments.txt
+// To test: curl http://localhost:8000/comments.csv
+// To test: curl http://localhost:8000/comments/1
+// To test: curl http://localhost:8000/comments/1.json
+// To test: curl http://localhost:8000/comments/1.xml
+// To test: curl http://localhost:8000/comments/1.html
+// To test: curl http://localhost:8000/comments/1.txt
+// To test: curl http://localhost:8000/comments/1.csv
+// To test: curl -X PUT -d "comment=Hello World" http://localhost:8000/comments/1
+// To test: curl -X PUT -d "comment=Hello World" http://localhost:8000/comments/1.json
+// To test: curl -X PUT -d "comment=Hello World" http://localhost:8000/comments/1.xml
+// To test: curl -X PUT -d "comment=Hello World" http://localhost:8000/comments/1.html
+// To test: curl -X PUT -d "comment=Hello World" http://localhost:8000/comments/1.txt
+// To test: curl -X PUT -d "comment=Hello World" http://localhost:8000/comments/1.csv
+// To test: curl -X DELETE http://localhost:8000/comments/1
+// To test
